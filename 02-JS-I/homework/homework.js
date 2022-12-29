75dad68789d03470f1a4175ba74d7222e53cd975
Agusnine <federicoagustingoy@gmail.com>
2022-12-29 15:17:58 -0300

    avance test 7

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -136,18 +136,21 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  return Math.pow ( num, 2 ) ;
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  return Math.pow ( num, 3 ) ;
   
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  return  Matemáticas . pow ( número ,  2 ) ;
   
 }
 
