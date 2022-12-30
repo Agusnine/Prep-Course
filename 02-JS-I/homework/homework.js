b8911d2cd3834a4d4b983935ba42d537b5620efb
Agusnine <federicoagustingoy@gmail.com>
2022-12-30 08:25:36 -0300

    avance test 9

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -179,26 +179,23 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if ( numero === 0 )  return false  ;
-
-else if ( numero < 0 )  return 'es negativo'  ;
-
-else ( numero > 0 ) ; return 'es positivo' 
-
-
+  
+  
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str  +  '!' ;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return ('federico' + ' agustin') ;
+  var combinados = 'nombre' + ' apellido' ;
+  return combinados ;
   
 }
 
@@ -206,6 +203,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+
   
 }
 
