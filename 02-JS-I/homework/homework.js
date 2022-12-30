5eb77184313a8fa100e36e314da28a9706ff645a
Agusnine <federicoagustingoy@gmail.com>
2022-12-30 09:40:29 -0300

    avance test 11

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -194,7 +194,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinados = 'nombre' + ' apellido' ;
+  var combinados = nombre + ' ' + apellido ; 
   return combinados ;
   
 }
@@ -203,7 +203,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + nombre + '!' ;
+  var obtenerSaludo = 'Hola ' + nombre + '!'
+  return obtenerSaludo ;
 
   
 }
@@ -211,13 +212,16 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return alto * ancho ;
   
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
-  //Escribe tu código aquí
+  //Escribe tu código 
+  return lado * 4 ;
+  
   
 }
 
@@ -225,6 +229,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+return (base * altura) / 2 ;
 
 }
 
@@ -233,6 +238,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  var cambio = euro * (120 / 100) ;
+  return cambio ;
+
   
 }
 
