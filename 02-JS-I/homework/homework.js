c4ed94d448e047df3cb2998ced02b50a5ca21b47
Agusnine <federicoagustingoy@gmail.com>
2022-12-30 09:01:04 -0300

    avance test 10

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -203,6 +203,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  return 'Hola' + nombre + '!' ;
 
   
 }
