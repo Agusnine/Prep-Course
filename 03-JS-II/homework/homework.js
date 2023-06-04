f86d845a85db49514d3268a3fe5ce73a59ceb508
Agusnine <federicoagustingoy@gmail.com>
2023-06-03 22:37:30 -0300

    test13

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -185,7 +185,12 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+ let arraytablaDel6 =[]
+  for(let i=0;i<11;i++)
+  {
+  arraytablaDel6.push (i*6)
+}
+  return arraytablaDel6
 }
 
 function tieneTresDigitos(numero){
